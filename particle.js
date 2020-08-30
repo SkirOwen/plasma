a15df1e3411bea26d483a32ab4426f80a3a32e64
Owen <owen.allemang@gmail.com>
2020-08-30 15:45:50 +0200

    implementing charges

--- particle.js
+++ particle.js
@@ -1,7 +1,22 @@
-function Circle(x, y, r) {
+function Particle(x, y, r, fixed, q) {
     var option = {
         friction: 0,
+        frictionAir: 0,
         restitution: 0.6,
+        isStatic: fixed,
+        charge: q,
+        // plugin: {
+        //     attractors: [
+        //         function(bodyA, bodyB) {
+        //             var force = {
+        //                 x: (),
+        //                 y: (),
+        //             };
+        //
+        //             // Apply force to both bodies
+        //         }
+        //     ]
+        // }
     }
     this.body = Bodies.circle(x, y, r, option);
     this.r = r;
@@ -29,7 +44,7 @@ function Circle(x, y, r) {
         stroke(255);
         fill(127);
         ellipse(0, 0, this.r*2);
-
+        line(0, 0, this.r, 0);
         pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,46 +1,87 @@
-// Matter.use('matter-attractors');
+Matter.use(
+    'matter-attractors' // PLUGIN_NAME
+);
 
 // module aliases
 var Engine = Matter.Engine,
     // Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint;
 
 var engine;
 var world;
-var circles = [];
+var particles = [];
 var boundaries = [];
 
 var ground;
 
 function setup() {
-    createCanvas(400, 400);
+    var canvas = createCanvas(500, 500);
     engine = Engine.create();
     world = engine.world;
+    world.gravity.scale = 0;
     // I'll need to try if this will work or not
     // Engine.run(engine);
 
-    boundaries.push(new Boundary(250, 300, width*0.6, 20, -0.3));
-    boundaries.push(new Boundary(150, 200, width*0.6, 20, 0.3));
+    var prev = null;
+    for (var x = 200; x < 400; x += 20) {
 
+        var fixed = false;
+        if (!prev) {
+            fixed = true;
+        }
+        var p = new Particle(x, 100, 10, fixed, -1);
+        // var p2 = new Particle(200, 150, 10);
+        particles.push(p);
+
+        if (prev) {
+            var option = {
+                bodyA: p.body,
+                bodyB: prev.body,
+                length: 20,
+                stiffness: 0.4,
+            }
+            var constraint = Constraint.create(option);
+            World.add(world, constraint);
+        }
+        prev = p;
+    }
+
+    boundaries.push(new Boundary(200, height, width, 30, 0));
+
+    var canvasmouse = Mouse.create(canvas.elt);
+    canvasmouse.pixelRatio = pixelDensity();
+    var option = {
+        mouse: canvasmouse,
+    }
+    mConstraint = MouseConstraint.create(engine, option);
+    World.add(world, mConstraint);
 }
 
 function mouseDragged() {
-    circles.push(new Circle(mouseX, mouseY, random(5, 10)));
+    // circles.push(new Circle(mouseX, mouseY, random(5, 10)));
 }
 
 function draw() {
     background(51);
     Engine.update(engine);
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].show();
-        if (circles[i].isOffScreen()) {
-            circles[i].removeFromWorld();
-            circles.splice(i, 1);
-            i--;
-        }
-    }
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].show();
     }
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].show();
+    }
+
+    if (mConstraint.body) {
+        var pos = mConstraint.body.position;
+        var offset = mConstraint.constraint.pointB;
+        var m = mConstraint.mouse.position;
+        stroke(0, 255, 0);
+        line(pos.x + offset.x, pos.y + offset.y, m.x, m.y);
+    }
+    // line(particles[0].body.position.x, particles[0].body.position.y,
+    //      particles[1].body.position.x, particles[1].body.position.y);
 }
